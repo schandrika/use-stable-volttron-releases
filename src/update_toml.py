475541beb13d27f4709c94e1f32872dcb51caeea
Chandrika Sivaramakrishnan <chandrika@pnnl.gov>
2024-02-20 16:39:35 -0800

    using pygithub

--- src/update_toml.py
+++ src/update_toml.py
@@ -1,14 +1,25 @@
 import toml
-with open('pyproject.toml', 'r') as f:
-    toml_dict = toml.load(f)
-dependencies_ = toml_dict['tool']['poetry']['dependencies']
-for lib in dependencies_:
-    if lib.startswith("fake"):
-        # if it has a pre-release=true flag, remove that flag
-        if isinstance(dependencies_[lib], dict) and dependencies_[lib].get('allow-prereleases'):
-            #del lib_details['allow-prereleases']  # this create dependencies.volttron-something as a new config entry
-            # so use just version. Could switch to tomllib with python 3.11
-            dependencies_[lib] = dependencies_[lib]["version"]
+from github import Github
+import os
 
-with open('new_toml_file.toml', 'w') as f:
-    toml.dump(toml_dict, f)
+def main():
+    client = Github(os.environ["GITHUB_TOKEN"])
+    client.get_repo(os.environ["GITHUB_REPOSITORY"])
+
+    with open('pyproject.toml', 'r') as f:
+        toml_dict = toml.load(f)
+    dependencies_ = toml_dict['tool']['poetry']['dependencies']
+    for lib in dependencies_:
+        if lib.startswith("fake"):
+            # if it has a pre-release=true flag, remove that flag
+            if isinstance(dependencies_[lib], dict) and dependencies_[lib].get('allow-prereleases'):
+                # del lib_details['allow-prereleases']  # this create dependencies.volttron-something as a new config entry
+                # so use just version. Could switch to tomllib with python 3.11
+                dependencies_[lib] = dependencies_[lib]["version"]
+
+    with open('new_toml_file.toml', 'w') as f:
+        toml.dump(toml_dict, f)
+
+
+if __name__ == "__main__":
+    main()
